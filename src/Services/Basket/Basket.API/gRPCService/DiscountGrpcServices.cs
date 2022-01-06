95acad4e2ea2baf70b40ffbd22cf84ed29f68023
Jeevananthan-23 <Jeeva023@outlook.com>
2022-01-07 01:09:30 +0530

    Discount.gRPC_dockerfile added

---
+++ src/Services/Basket/Basket.API/gRPCService/DiscountGrpcServices.cs
@@ -0,0 +1,23 @@
+﻿using Discount.gRPC.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.grpcServices
+{
+    public class DiscountGrpcService
+    {
+        private readonly DiscountProtoService.DiscountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DiscountProtoService.DiscountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+
+            return await _discountProtoService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
